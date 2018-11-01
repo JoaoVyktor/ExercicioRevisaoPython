2561e23194a89c52f3bccba55bef82481c847022
JoaoVyktor <42653208+JoaoVyktor@users.noreply.github.com>
2018-11-01 11:50:46 -0300

    Update primeira questão.py

--- primeira questão.py
+++ primeira questão.py
@@ -1,1 +1,16 @@
-dadad
+nota1 =  float(input())
+nota2 =  float(input())
+nota3 =  float(input())
+media = (nota1+nota2+nota3)/3
+
+if(media>=7):
+    print("Aprovado")
+    print("{:.1f}".format(media))
+
+elif(media<7):
+    print("Reprovado")
+    print("{:.1f}".format(media))
+
+elif(media==10):
+    print("Aprovado com distinção")
+    print("{:.1f}".format(media))
